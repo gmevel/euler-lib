971a5e6734084112416eddbd09645f2a7c83c370
Glen Mével <glen.mevel@crans.org>
2018-08-11 18:06:40 +0200

    overflowing arithmetic

--- Arith.ml
+++ Arith.ml
@@ -1,9 +1,104 @@
 exception Overflow
 
+exception Division_not_exact
+
 let sign a =
   if a = 0      then   0
   else if a > 0 then ~+1
   else               ~-1
+
+let ( ~-? ) = ( ~- )
+
+(* Number of bits of an unsigned integer (OCaml integers are one bit less than
+ * machine words, and there is one sign bit). *)
+let uint_size = Sys.word_size - 2
+
+let add_nonneg =
+  let highest_bit = 1 lsl (uint_size - 1) in
+fun a b ->
+  assert (0 <= a) ;
+  assert (0 <= b) ;
+  if a land b land highest_bit <> 0 then
+    raise Overflow
+  else if (a lor b) land highest_bit = 0 then
+    a + b
+  else begin
+    let s = a + b in
+    if s land highest_bit = 0 then
+      raise Overflow
+    else
+      s
+  end
+
+let ( +? ) a b =
+  assert (a <> min_int) ;
+  assert (b <> min_int) ;
+  if a > 0 && b > 0 then
+    add_nonneg a b
+  else if a < 0 && b < 0 then
+    ~- (add_nonneg ~-a ~-b)
+  else
+    a + b
+
+let ( -? ) a b =
+  assert (a <> min_int) ;
+  assert (b <> min_int) ;
+  if a > 0 && b < 0 then
+    add_nonneg a ~-b
+  else if a < 0 && b > 0 then
+    ~- (add_nonneg ~-a b)
+  else
+    a - b
+
+let mul_nonneg =
+  let uint_half_size = uint_size / 2 in
+  let lower_half = (1 lsl uint_half_size) - 1 in
+fun a b ->
+  assert (0 <= a) ;
+  assert (0 <= b) ;
+  let (ah, al) = (a lsr uint_half_size, a land lower_half)
+  and (bh, bl) = (b lsr uint_half_size, b land lower_half) in
+  if ah <> 0 && bh <> 0 then
+    raise Overflow
+  else begin
+    let al_bl = al*bl in
+    let (h, l) = (al_bl lsr uint_half_size, al_bl land lower_half) in
+    (* This expression does not overflow (at most one of ah×bl and al×bh is
+     * non‐null; and each variable is at most equal to lower_half = 2^{N∕2}−1
+     * where N = uint_size, so that the total is at most equal to
+     * lower_half² + lower_half, which is less than 2^N): *)
+    let h' = ah*bl + al*bh + h in
+    if h' > lower_half then
+      raise Overflow
+    else
+      (h' lsl uint_half_size) lor l
+  end
+
+let ( *? ) a b =
+  assert (a <> min_int) ;
+  assert (b <> min_int) ;
+  if a >= 0 && b >= 0 then
+    mul_nonneg a b
+  else if a <= 0 && b <= 0 then
+    mul_nonneg ~-a ~-b
+  else if a > 0 (* && b < 0 *) then
+    ~- (mul_nonneg a ~-b)
+  else (* a < 0 && b > 0 *)
+    ~- (mul_nonneg ~-a b)
+(* This is made shorter using a bitwise trick, but it may not be faster because
+ * of calls to [abs]. TODO: Benchmark it. *)
+(*
+let ( *? ) a b =
+  assert (a <> min_int) ;
+  assert (b <> min_int) ;
+  if a lxor b >= 0 then
+    mul_nonneg (abs a) (abs b)
+  else
+    ~- (mul_nonneg (abs a) (abs b))
+*)
+
+let pow =
+  Common.pow ~mult:( *? ) ~unit:1
 
 let ediv a b =
   let q = a / b
@@ -35,7 +130,6 @@ let rec gcd a b =
   else
     gcd b (a mod b)
 
-(* TODO: detect overflows *)
 let gcdext a0 b0 =
   let rec gcdext a b u v x y =
     assert (a = u*a0 + v*b0) ;
@@ -47,17 +141,21 @@ let gcdext a0 b0 =
         (~-a, ~-u, ~-v)
     end else begin
       let q = a / b in
-      gcdext b (a mod b) x y (u-q*x) (v-q*y)
+      (* TODO: Avoid overflows in intermediate values for computing the Bézout
+       * coefficients (use zarith? prove that somehow we compute the smallest
+       * coefficients possible and that there are in fact no overflows?). *)
+      gcdext b (a mod b) x y (u -? q *? x) (v -? q *? y)
     end
   in
   gcdext a0 b0 1 0 0 1
 
-(* TODO: detect overflows *)
 let lcm a b =
+  assert (a <> min_int) ;
+  assert (b <> min_int) ;
   if a = 0 || b = 0 then
     0
   else
-    a / gcd a b * b
+    a / gcd a b *? b
 
 let rec valuation ~factor:d n =
   assert (abs d <> 1) ;
@@ -68,15 +166,19 @@ let rec valuation ~factor:d n =
     let (k, n') = valuation ~factor:d (n / d) in
     (k+1, n')
 
+(*
 let valuation_of_2 n =
-  (*assert (n <> 0) ;
+  assert (n <> 0) ;
   let k = ref 0 in
   let m = ref n in
   while !m mod 2 = 0 do
     incr k ;
     m := !m / 2 ;
   done ;
-  (!k, !m)*)
+  (!k, !m)
+*)
+(* Made potentially faster with a trick. TODO: Benchmark it. *)
+let valuation_of_2 n =
   assert (n <> 0) ;                  (*    n = 0b ???????10000 *)
   let bits = (n lxor (n-1)) lsr 1 in (* bits = 0b 000000001111 *)
   let k = snd@@frexp (float bits) in (* we convert to float and get the exponent *)
@@ -86,15 +188,35 @@ let is_square n =
   let r = truncate @@ sqrt @@ float n in
   r*r = n
 
-(* TODO: detect overflows *)
-let mul_div a b d =
-  assert (0 <= d) ;
-  assert (0 <= a && 0 <= b) ;
-  let (qa, a') = (a / d, a mod d) in
-  let (qb, b') = (b / d, b mod d) in
-  qa*b + a'*qb + a'*b'/d
+let mul_div_exact a b d =
+  if d = 0 then
+    raise Division_by_zero ;
+  let g = gcd a d in
+  let (a, d) = (a / g, d / g) in
+  let g = gcd b d in
+  let (b, d) = (b / g, d / g) in
+  if abs d <> 1 then
+    raise Division_not_exact
+  else
+    a *? b
 
-(* TODO: detect overflows *)
+let mul_quo a b d =
+  (* This will be checked anyway by following native divisions: *)
+  (*if d = 0 then
+    raise Division_by_zero ;*)
+  let s = sign a * sign b * sign d
+  and a = abs a
+  and b = abs b
+  and d = abs d in
+  let g = gcd a d in
+  let (a, d) = (a / g, d / g) in
+  let g = gcd b d in
+  let (b, d) = (b / g, d / g) in
+  let (qa, ra) = (a / d, a mod d) in
+  let (qb, rb) = (b / d, b mod d) in
+  s * ((qa *? b) +? (ra *? qb) +? (ra *? rb / d))
+  (* TODO: Avoid overflow in the intermediate product (use zarith?). *)
+
 let binoms n =
   assert (0 <= n) ;
   (* Uses the formula: binom(n,p) = binom(n,p−1) × (n−p+1) ∕ p *)
@@ -102,22 +224,11 @@ let binoms n =
   b.(0) <- 1 ;
   b.(n) <- 1 ;
   for k = 1 to n/2 do
-    (* (1) Overflow‐free as long as the expected result is less than max_int ∕ k
-     *     [ overall, this makes the algorithm overflow‐free as long as the
-     *     central binomial coefficient is less than max_int ∕ (n∕2);
-     *     for 64‐bit OCaml, this is valid for n∕2 ≤ 30 ]: *)
-    (*b.(k) <- b.(k-1) * (n-k+1) ∕ k ;*)
-    (* (2) Overflow‐free as long as the expected result is less than max_int,
-     *     and that k < √max_int [ overall, this makes the algorithm
-     *     overflow‐free as long as the result itself is less than max_int,
-     *     because for n large enough we have that n∕2 < √ binom(n, n∕2);
-     *     for 64‐bit OCaml, this is valid for n∕2 ≤ 32 ]: *)
-    b.(k) <- mul_div b.(k-1) (n-k+1) k ;
+    b.(k) <- mul_div_exact b.(k-1) (n-k+1) k ;
     b.(n-k) <- b.(k)
   done ;
   b
 
-(* TODO: detect overflows *)
 let binom n p =
   assert (0 <= p && p <= n) ;
   (* Uses the formula: binom(n,p) = binom(n−1,p−1) × n ∕ p *)
@@ -125,31 +236,18 @@ let binom n p =
   let m = n - p in
   let c = ref 1 in
   for k = 1 to p do
-    (* (1) Overflow‐free as long as the expected result is less than max_int ∕ k
-     *     [ overall, this makes the algorithm overflow‐free as long as the
-     *     result is less than max_int ∕ q, where q = min(p, n−p) ]: *)
-    (*c := !c * (m + k) ∕ k*)
-    (* (1b) Avoids a few more overflows: *)
-    (*c := !c * m ∕ k + !c ;*)
-    (* (2) Overflow‐free as long as the expected result is less than max_int,
-     *     and that k < √max_int [ overall, this makes the algorithm
-     *     overflow‐free as long as the result itself is less than max_int,
-     *     because for n large enough we have that q < √ binom(n, q) ]: *)
-    c := mul_div !c (m+k) k ;
+    c := mul_div_exact !c (m+k) k ;
   done ;
   !c
 
-(* TODO: detect overflows *)
 let central_binom p =
   assert (0 <= p) ;
   (* Uses the formula: binom(2p,p) = binom(2(p−1),p−1) × 2(2p−1) ∕ p *)
   let c = ref 1 in
   for k = 1 to p do
-    (*c := !c * 2 * (2*k - 1) ∕ k*)
-    (* (2) Avoids all overflows, except for the result itself [ overall, this
-     *     makes the algorithm overflow‐free as long as the result itself is
-     *     less than max_int; for 64‐bit OCaml, this is valid for p ≤ 32 ] *)
-    c := !c * 4  -  !c * 2 / k
+    (*c := mul_div_exact !c (2 * (2*k - 1)) k*)
+    (* This expression already avoids all overflows except for the result: *)
+    c := !c *? 4  -  !c * 2 / k
   done ;
   !c
 
--- Arith.mli
+++ Arith.mli
@@ -1,10 +1,23 @@
-(** Arithmetic on native integers. *)
+(** Arithmetic on overflowing integers. *)
 
 (**
-    All operations defined here act on native integers (type [int]). Either they
-    are free of overflows, or they raise [Overflow] when their {e result} would
-    exceed the capacity of native integers (and only in that case). Functions
-    which can overflow are signaled explicitely in this documentation.
+    All operations defined here act on overflowing integers. An overflowing
+    integer is any native integer (type [int]) except [min_int]. Otherwise said,
+    they are the integers whose absolute value is at most
+    [max_int] = 2{^[int_size]−1} − 1. This makes the range of overflowing
+    integers symmetrical, so that computing the opposite [( ~- )] or the
+    absolute value [abs] never overflows (the presence of an additional value
+    2{^[int_size]−1} being arbitrarily interpreted as a negative value fits the
+    modulo semantics of integers, but is alien to an overflowing semantics).
+    This allows to use [min_int] as a special value, for example to signal that
+    an overflow occured. Here, we rather raise an exception for that purpose.
+    All functions in this library may fail when given [min_int] where an
+    overflowing integer was expected.
+
+    All operations defined here either are free of overflows, or raise
+    [Overflow] when their {e result} would exceed the range of overflowing
+    integers (and only in that case). Functions which can overflow are signaled
+    explicitely in this documentation.
 
     Functions in this library may raise [Assert_failure], instead of the more
     traditional [Invalid_argument], when some precondition is not met. This is
@@ -17,10 +30,13 @@
     constant time or constant space is implied.
  *)
 
-(** Raised when the result of an operation exceeds the capacity of native
+(** Raised when the result of an operation exceeds the range of overflowing
     integers. *)
 exception Overflow
 
+(** Raised when an operation was expected to perform an exact division but the
+    dividend was not a multiple of the divisor. *)
+exception Division_not_exact
 
 (******************************************************************************)
 
@@ -30,19 +46,54 @@ exception Overflow
     null. *)
 val sign : int -> int
 
+(** Integer opposite. This is the same as [( ~- )]. It cannot overflow. *)
+val ( ~-? ) : int -> int
+
+(** Overflowing integer addition.
+    @raise Overflow when the result exceeds the range of overflowing integers. *)
+val ( +? ) : int -> int -> int
+
+(** Overflowing integer substraction.
+    @raise Overflow when the result exceeds the range of overflowing integers. *)
+val ( -? ) : int -> int -> int
+
+(** Overflowing integer multiplication.
+    @raise Overflow when the result exceeds the range of overflowing integers. *)
+val ( *? ) : int -> int -> int
+
+(** Overflowing integer exponentiation. [pow a n] is [a] to the power [n],
+    provided that [n] is non‐negative.
+    {b Complexity:} 𝒪(log([n])) integer multiplications.
+    @raise Overflow when the result exceeds the range of overflowing integers. *)
+val pow : int -> int -> int
+
 (** [ediv a b] is the Euclidean division of [a] by [b]; it returns [(q, r)] such
     that [a] = [b]×[q] + [r] and 0 ≤ [r] < [b].
-    @raise Division_by_zero if [b] is null. *)
+    @raise Division_by_zero when [b] is null. *)
 val ediv : int -> int -> int * int
 
 (** [equo a b] is the quotient of the Euclidean division of [a] by [b].
-    @raise Division_by_zero if [b] is null. *)
+    @raise Division_by_zero when [b] is null. *)
 val equo : int -> int -> int
 
 (** [erem a b] is the remainder of the Euclidean division of [a] by [b].
-    @raise Division_by_zero if [b] is null. *)
+    @raise Division_by_zero when [b] is null. *)
 val erem : int -> int -> int
 
+(** [mul_div_exact a b d] computes [a]×[b]∕[d] when [d] does divide [a]×[b].
+    @raise Division_by_zero when [d] is null.
+    @raise Division_not_exact when [d] does not divide [a]×[b].
+    @raise Overflow when the result exceeds the range of overflowing integers. *)
+val mul_div_exact : int -> int -> int -> int
+
+(** [mul_quo a b d] tries to compute [a]×[b]÷[d]. It can overflow even if the
+    final result fits in the range of overflowing integers. This case is
+    guaranteed not to happen as long the denominator of the reduced fraction is
+    less than √[max_int] (in particular, when [d] is less than √[max_int]).
+    {e This should be fixed, but I don’t know how.}
+    @raise Division_by_zero when [d] is null.
+    @raise Overflow as described. *)
+val mul_quo : int -> int -> int -> int
 
 (******************************************************************************)
 
@@ -55,22 +106,26 @@ val gcd : int -> int -> int
 
 (** [gcdext a b] is the extended Euclidean algorithm; it returns [(d, u, v)]
     where [d] is the {e positive} greatest common divisor of [a] and [b], and
-    [u] and [v] are Bézout integers, such that [u]×[a] + [v]×[b] = [d].
+    [u] and [v] are Bézout’s coefficients, such that [u]×[a] + [v]×[b] = [d].
     {b Complexity:} 𝒪(log(min([a],[b]))) integer divisions.
-    @return [d] = 0 only when [a] = [b] = 0. *)
+    @return [d] = 0 only when [a] = [b] = 0.
+    @raise Overflow when the computation of the Bézout’s coefficients provokes
+    an overflow, even if there exists a pair of Bézout coefficients which would
+    fit in the range of our overflowing integers. {e This should be fixed, but I
+    don’t know how.} *)
 val gcdext : int -> int -> int * int * int
 
 (** [lcm a b] is the lesser common multiple of [a] and [b]. Its sign is that of
     [a]×[b].
     {b Complexity:} 𝒪(log(min([a],[b]))) integer divisions.
-    @raise Overflow if the result exceeds the capacity of native integers. *)
+    @raise Overflow when the result exceeds the range of overflowing integers. *)
 val lcm : int -> int -> int
 
 (** [valuation ~factor:d n] returns [(k, m)] such that [n] = [d]{^[k]}×[m] and
     [m] is not divisible by [d]. This assumes that [n] is not null and that [d]
     is not ±1.
     {b Complexity:} 𝒪([k]) = 𝒪(log([n])) integer divisions.
-    @raise Division_by_zero if [d] is null. *)
+    @raise Division_by_zero when [d] is null. *)
 val valuation : factor:int -> int -> int * int
 
 (** [valuation_of_2] is the same as [valuation ~factor:2]. It may be faster. *)
@@ -83,29 +138,22 @@ val is_square : int -> bool
 
 (** {2 Binomial coefficients } *)
 
-(** [mul_div a b d] computes [a]×[b]∕[d], provided that [d], [a] and [b] are
-    non‐negative. It does not overflow as long as the expected result is less
-    than [max_int] and that [d] is less than √[max_int].
-    @raise Division_by_zero if [d] is null.
-    @raise Overflow as explained. *)
-val mul_div : int -> int -> int -> int
-
 (** [binoms n] returns the [n]{^th} row of Pascal’s triangle, provided that [n]
     is a non-negative integer.
     {b Complexity:} time 𝒪([n]), space 𝒪([n]).
-    @raise Overflow if the greatest value of the result exceeds the capacity of
-    native integers. For a 64‐bit version of OCaml, this happens for [n] ≥ 66. *)
+    @raise Overflow when the greatest value of the result exceeds the range of
+    overflowing integers. For 64‐bit OCaml, this happens for [n] ≥ 66. *)
 val binoms : int -> int array
 
 (** [binom n p] is the [p]{^th} element of the [n]{^th} row of Pascal’s
     triangle, provided that 0 ≤ [p] ≤ [n].
     {b Complexity:} time 𝒪(min([p],[n]−[p])) = 𝒪([n]), space 𝒪(1).
-    @raise Overflow if the result exceeds the capacity of native integers. *)
+    @raise Overflow when the result exceeds the range of overflowing integers. *)
 val binom : int -> int -> int
 
 (** [central_binom p] is the [p]{^th} element of the 2×[p]{^th} row of Pascal’s
     triangle, provided that 0 ≤ [p].
     {b Complexity:} time 𝒪([p]), space 𝒪(1).
-    @raise Overflow if the result exceeds the capacity of native integers.
-    For a 64‐bit version of OCaml, this happens for [p] ≥ 33. *)
+    @raise Overflow when the result exceeds the range of overflowing integers.
+    For 64‐bit OCaml, this happens for [p] ≥ 33. *)
 val central_binom : int -> int
