947325f14002a4f3afd94dc96c5de3bb2a16fcaa
Glen Mével <glen.mevel@crans.org>
2018-08-13 00:12:04 +0200

    fixed Common.pow (was causing spurious overflows)

--- Common.ml
+++ Common.ml
@@ -1,11 +1,20 @@
-let rec pow ~mult ~unit b n =
+(* This implementation avoids an unnecessary squaring (when used with
+ * overflowing integers, this could provoke a spurious overflow). *)
+let pow ~mult ~unit =
+  let rec pow ~acc b n =
+    if n land 1 = 0 then
+      pow ~acc (mult b b) (n lsr 1)
+    else if n = 1 then
+      mult acc b
+    else
+      pow ~acc:(mult acc b) (mult b b) (n lsr 1)
+  in
+fun b n ->
   assert (n >= 0) ;
   if n = 0 then
     unit
-  else if n mod 2 = 0 then
-    pow ~mult ~unit (mult b b) (n / 2)
   else
-    pow ~mult ~unit:(mult unit b) (mult b b) (n / 2)
+    pow ~acc:unit b n
 
 let rec memoized_fix f =
   let mem = Hashtbl.create 1 in
