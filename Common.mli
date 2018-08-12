a57812803e04e3ba74388f7e40c6e42208ccf3fb
Glen Mével <glen.mevel@crans.org>
2018-08-12 10:04:12 +0200

    improved documentation for Common.pow

--- Common.mli
+++ Common.mli
@@ -3,22 +3,21 @@
 (******************************************************************************)
 
 (** Generic fast exponentiation.
-    [pow ~mult ~unit x n] is [unit] composed [n] times with [x], provided that
-    [n] is non‐negative. For example:
-    {[
+    [pow ~mult ~unit x n] is [unit] composed [n] times to the right with [x],
+    provided that [n] is non‐negative. For example:
+{[
     pow ~mult:(^) ~unit:"x" "y" 5
 ]}
     yields ["xyyyyy"].
     {b Complexity:} 𝒪(log([n])) calls to [mult].
     @param mult the composition operator; should be associative.
-    @param unit the neutral element (or at least initial accumulator; note that
-    the implementation compose items from left to right, so that [unit] is the
-    left‐most operand of the product). *)
+    @param unit the left‐most operand of the product (most often, we use a
+    neutral element for [mult]). *)
 val pow : mult:('a -> 'a -> 'a) -> unit:'a -> 'a -> int -> 'a
 
 (** Memoizing fixpoint combinator.
     Example use:
-    {[
+{[
     let fib = memoized_fix\@\@fun fib n ->
       if n < 2 then
         1
